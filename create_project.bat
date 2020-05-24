e87b5ad81cb4ca36d371a3abf8a0f1a63568aaf2
Sarat Angajala <chandrasarat74@gmail.com>
2020-05-24 19:26:10 +0530

    Few minor bug fixes

--- create_project.bat
+++ create_project.bat
@@ -1,15 +1,17 @@
 @Echo off
-set CURRPATH=%CD%
+set CURRPATH=D:\Academics\Learning\test_runs\Automation
 if %1 == -l goto learning
 if %1 == -p goto project
 Echo Argument action not found
 pause
 goto end
 :learning
-cd D:/Academics/Learning/test_runs/
+d:
+cd Academics/Learning/test_runs/
 goto continue
 :project
-cd D:/Academics/Projects/
+d:
+cd Academics/Projects/
 :continue
 mkdir %2
 cd %2
@@ -21,6 +23,7 @@ git commit -m "Initial Commit"
 python D:\Academics\Learning\test_runs\Automation\github.py %2 %CURRPATH%
 git push origin master
 cd %CURRPATH%
+d:
 code %NEWPATH%
 :end
 pause
